f63264e9a454534f025486ed0aea35e157e7dff6
carlijnweijer <carlijnweijer@gmail.com>
2020-11-24 17:16:25 +0100

    set up relations between users & answers and users & questions

--- models/user.js
+++ models/user.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      user.hasMany(models.answer);
+      user.hasMany(models.question);
     }
   }
   user.init(
