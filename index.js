db2fb5df07cd59b8913cfbba7b9a05280938fabb
carlijnweijer <carlijnweijer@gmail.com>
2020-11-25 18:50:03 +0100

    improved file structure, moved constants to seperate file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const { PORT } = require("./config/constants");
 
 const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
