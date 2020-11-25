795d405d96a3aa21d8ca4c3b92015144aaea257e
carlijnweijer <carlijnweijer@gmail.com>
2020-11-25 19:02:09 +0100

    Add signup endpoint and add me endpoint

--- routers/auth.js
+++ routers/auth.js
@@ -24,11 +24,50 @@ router.post("/login", async (req, res, next) => {
       });
     }
 
-    const jwt = toJWT({ userId: user.id });
-    return res.status(200).send({ jwt });
+    delete user.dataValues["password"]; // don't send back the password hash
+    const token = toJWT({ userId: user.id });
+    return res.status(200).send({ token, ...user.dataValues });
   } catch (error) {
     return res.status(400).send({ message: "Something went wrong, sorry" });
   }
+});
+
+router.post("/signup", async (req, res) => {
+  const { email, password, name } = req.body;
+  if (!email || !password || !name) {
+    return res.status(400).send("Please provide an email, password and a name");
+  }
+
+  try {
+    const newUser = await User.create({
+      email,
+      password: bcrypt.hashSync(password, SALT_ROUNDS),
+      name,
+    });
+
+    delete newUser.dataValues["password"]; // don't send back the password hash
+
+    const token = toJWT({ userId: newUser.id });
+
+    res.status(201).json({ token, ...newUser.dataValues });
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      return res
+        .status(400)
+        .send({ message: "There is an existing account with this email" });
+    }
+
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+// The /me endpoint can be used to:
+// - get the users email & name using only their token
+// - checking if a token is (still) valid
+router.get("/me", authMiddleware, async (req, res) => {
+  // don't send back the password hash
+  delete req.user.dataValues["password"];
+  res.status(200).send({ ...req.user.dataValues });
 });
 
 module.exports = router;
