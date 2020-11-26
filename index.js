571c1dc8ea0d1bbddad0000f29c7888a1d0e37d2
carlijnweijer <carlijnweijer@gmail.com>
2020-11-26 17:08:32 +0100

    Add cors middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const corsMiddleWare = require("cors");
 const { PORT } = require("./config/constants");
 
 const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
+app.use(corsMiddleWare());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
