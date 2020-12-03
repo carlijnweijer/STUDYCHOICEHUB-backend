17dc013caed9954d69946a0ae9b8a5740908d9d0
carlijnweijer <carlijnweijer@gmail.com>
2020-12-03 12:51:09 +0100

    config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    use_env_variable: "DATABASE_URL",
+    url: process.env.ELEPHANT_SQL,
+    dialect: "postgres",
+    dialectOptions: {
+      statement_timeout: 1000,
+      idle_in_transaction_session_timeout: 5000,
+    },
   },
   test: {
     username: "root",
@@ -11,10 +16,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
   },
 };
